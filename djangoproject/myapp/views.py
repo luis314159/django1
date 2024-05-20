cd2b203aa151bbe9575259f3db293c17290a8995
luis314159 <luis.14xbox@live.com.mx>
2024-05-20 08:54:34 -0600

    Admin pannel

--- djangoproject/myapp/views.py
+++ djangoproject/myapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
-def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def index(request):
+    return render(request, 'index.html')
+
+def hello(request, username ='no user name'):
+ 
+    return HttpResponse("<h1>Hello %s</h1>" %username)
 
 def about(request):
     return HttpResponse("<h1>About</h1>")
+
+def projects(request):
+    project = list(Project.objects.values())
+    return JsonResponse(project, safe = False)
+
+def tasks(request,title):
+    #task = Task.objects.get(id=id) 
+    task = get_object_or_404(Task, title=title)
+    #task = Task.objects.get(title=title) 
+    return HttpResponse('task: %s '% task.title)
